93f6f941e769a69bb07cbbea6f67410d2e02e76b
EvenhuisD <devenhuis81@gmail.com>
2024-04-09 22:08:38 +0200

    routes removed, added homeroute

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Home Page</div>
+</template>
+
+<script setup lang="ts">
+import {onMounted} from 'vue';
+
+onMounted(() => {
+    console.log('mounted homepage');
+});
+</script>
