4cedd48fb8e79b2adc0004e1d4a59cf3a17de292
EvenhuisD <devenhuis81@gmail.com>
2024-04-10 20:47:52 +0200

    route cleanup, test comp;onent to center canvas in container and title with absolute positioning centering around the middle

--- resources/js/routes.ts
+++ resources/js/routes.ts
@@ -1,92 +1,23 @@
 import type {RouteRecordRaw} from 'vue-router';
 
-import Home from 'pages/Home.vue';
-// import Index from 'pages/registrations/Overview.vue';
-// import Login from 'pages/auth/Login.vue';
-// import NewPassword from 'pages/auth/NewPassword.vue';
-// import Register from 'pages/users/Register.vue';
-// import RegisterAsAdmin from 'pages/users/RegisterAsAdmin.vue';
-// import RequestPasswordReset from 'pages/auth/RequestPasswordReset.vue';
+// import Home from 'pages/Home.vue';
+import Test from 'pages/Test.vue';
 
 export const routes: RouteRecordRaw[] = [
+    // {
+    //     path: '/',
+    //     component: Home,
+    //     name: 'Home',
+    //     meta: {
+    //         shouldBeLoggedIn: false,
+    //     },
+    // },
     {
         path: '/',
-        component: Home,
-        name: 'Home',
+        component: Test,
+        name: 'Test',
         meta: {
             shouldBeLoggedIn: false,
         },
     },
-    // {
-    //     path: '/request-password-reset',
-    //     component: RequestPasswordReset,
-    //     name: 'RequestPasswordReset',
-    //     meta: {
-    //         shouldBeLoggedIn: false,
-    //     },
-    // },
-    // {
-    //     path: '/reset-password/:token',
-    //     component: NewPassword,
-    //     name: 'NewPassword',
-    //     meta: {
-    //         shouldBeLoggedIn: false,
-    //     },
-    // },
-    // {
-    //     path: '/user-overview',
-    //     component: Overview,
-    //     name: 'Users.Overview',
-    //     meta: {
-    //         shouldBeLoggedIn: true,
-    //     },
-    // },
-    // {
-    //     path: '/registrations',
-    //     component: Index,
-    //     name: 'Registrations.Overview',
-    //     meta: {
-    //         shouldBeLoggedIn: true,
-    //     },
-    // },
-    // {
-    //     path: '/edit-user/:userId',
-    //     component: Edit,
-    //     name: 'Users.Edit',
-    //     meta: {
-    //         shouldBeLoggedIn: true,
-    //     },
-    // },
-    // {
-    //     path: '/register-admin/:hash',
-    //     component: RegisterAsAdmin,
-    //     name: 'Users.RegisterAsAdmin',
-    //     meta: {
-    //         shouldBeLoggedIn: false,
-    //     },
-    // },
-    // {
-    //     path: '/show-user/:userId',
-    //     component: Detail,
-    //     name: 'Users.Detail',
-    //     meta: {
-    //         shouldBeLoggedIn: true,
-    //     },
-    // },
-    // {
-    //     path: '/register-user',
-    //     component: Register,
-    //     name: 'Users.Register',
-    //     meta: {
-    //         shouldBeLoggedIn: true,
-    //     },
-    // },
-    // {
-    //     path: '/invitations',
-    //     component: Invitations,
-    //     name: 'Users.Invitations',
-    //     meta: {
-    //         shouldBeLoggedIn: true,
-    //     },
-    // },
 ];
