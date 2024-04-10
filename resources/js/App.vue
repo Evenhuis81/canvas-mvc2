6a8f2f9754078706b2e5d4f34b579b7cc24f0854
EvenhuisD <devenhuis81@gmail.com>
2024-04-10 20:38:09 +0200

    canvas module, stripped App.vue to bare, canvasOptions and Home component

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,40 +1,13 @@
 <template>
     <main>
-        <!-- <SideBar v-if="isLoggedIn" /> -->
         <RouterView />
     </main>
 </template>
 
-<script setup lang="ts">
-// import {isLoggedIn} from 'store/auth';
-// import SideBar from './pages/SideBar.vue';
-</script>
+<script setup lang="ts"></script>
 
-<!-- <style>
-html,
+<style>
 body {
-    height: 100%;
-    background-color: #f5f5f5;
-    align-items: center;
+    margin: 0;
 }
-main {
-    width: 100%;
-    display: flex;
-    flex-wrap: nowrap;
-    height: 100vh;
-    height: -webkit-fill-available;
-    max-height: 100vh;
-    overflow-x: auto;
-    overflow-y: hidden;
-}
-button {
-    margin: 4px;
-}
-.invalid-feedback {
-    width: 100%;
-    margin-top: 0.25rem;
-    margin-bottom: 0.5rem;
-    font-size: 80%;
-    color: #e3342f;
-}
-</style> -->
+</style>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,11 +1,27 @@
 <template>
-    <div>Home Page</div>
+    <div id="container" />
 </template>
 
 <script setup lang="ts">
 import {onMounted} from 'vue';
+import {setCanvas} from './canvas';
+
+const canvasOptions = {
+    width: 640,
+    height: 480,
+    backgroundColor: '#000',
+    position: 'center',
+};
 
 onMounted(() => {
-    console.log('mounted homepage');
+    const {context: ctx} = setCanvas(canvasOptions);
+
+    const animate = () => {
+        ctx.clearRect(0, 0, 649, 480);
+
+        requestAnimationFrame(animate);
+    };
+
+    animate();
 });
 </script>
