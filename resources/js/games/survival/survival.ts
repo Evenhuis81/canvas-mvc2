53bf318d957d271b7ba65887d7523676df4ef6ea
DojoEemsdelta <djo.app.eems@gmail.com>
2024-08-10 13:31:59 +0200

    gf

--- resources/js/games/survival/survival.ts
+++ resources/js/games/survival/survival.ts
@@ -1,48 +1,35 @@
-import button from 'library/button/button';
+// import button from 'library/button/button';
 import {initialize, resources} from 'library/index';
-import {ButtonOptions} from 'library/types/button';
+// import {ButtonOptions} from 'library/types/button';
 
 export default {
     setup: async () => {
         initialize('survival', {containerID: 'container', full: true, clear: true, bg: '#000'});
 
-        startLevel();
+        main();
     },
     run: () => resources.survival.engine.run(),
     runOnce: () => resources.survival.engine.runOnce(),
 };
 
-const startLevel = () => {
-    button.create('survival', startButton);
+const main = () => {
+    const {context, engine} = resources['survival'];
+
+    const update = () => {
+        //
+    };
+    const show = () => {
+        for (const a in asteroids) {
+            //
+        }
+    };
+
+    const asteroids: ReturnType<typeof createSpaceObject>[] = [];
+
+    asteroids.push(createSpaceObject(20, 20, 8, -6, 16));
+
+    // engine.setUpdate({fn: update});
+    engine.setShow({fn: show});
 };
 
-const startButton: ButtonOptions = {
-    id: 'start',
-    name: 'Start Button',
-    w: innerWidth * 0.1,
-    text: 'Start #',
-    click: {
-        down: evt => {
-            // console.log('down', evt);
-        },
-        up: evt => {
-            // console.log('up', evt);
-            evt.disable();
-            console.log('disabled');
-
-            setTimeout(() => {
-                evt.activate();
-                console.log('activated');
-            }, 1200);
-        },
-        end: evt => {
-            evt.selfDestruct();
-
-            startSurvival();
-        },
-    },
-};
-
-const startSurvival = () => {
-    console.log('start game');
-};
+const createSpaceObject = (x = 0, y = 0, dx = 0, dy = 0, size = 0) => ({x, y, dx, dy, size});
